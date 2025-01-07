7fb4d26d74986abb9075575b6587b387daa2ef41
Semih382 <24370601007@subu.edu.tr>
2025-01-07 13:50:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//dinamik menü elemanlarını oluşturma
+
+const menuItems=['Anasayfa','Hakkımızda','Hizmetler','İletişim'];
+
+const menu = document.getElementById('menu');
+
+const hamburger=document.querySelector('.hamburger');
+
+//menu elemanlarını döngü ile oluşturma
+
+menuItems.forEach(item=>{
+    const li=document.createElement('li');
+    li.textContent = item;
+    li.addEventListener('click', ()=>{
+        alert(`${item} - seçildi`);
+    });
+    menu.appendChild(li);
+});
+
+//hamburger menü aktif etme
+hamburger.addEventListener('click',()=>{
+    menu.classList.toggle('show');
+});
+
+
+//Tıklayınca Menü Kapansın
+menu.querySelectorAll('li').forEach(li=>{
+    li.addEventListener('click',()=>{
+        menu.classList.remove('show');
+    });
+});
